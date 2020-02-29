f6901e8b5b5e09480ae2a6897af7828d43e8587d
Your Name <you@example.com>
2020-02-29 12:32:50 +0530

    Ladder reached  exact position

--- Snake_And_Ladder.sh
+++ Snake_And_Ladder.sh
@@ -19,7 +19,7 @@ echo $randomNumber
 randomNumber1=$((RANDOM%3+1))
 
 # Check Snake position Ladder position and No Play
-while [[ $position -lt 100 ]]
+while [[ $position -ne 100 ]]
 do
 	case $randomNumber1 in
    	1)
@@ -30,8 +30,12 @@ do
    	2)
       	ladderRandom="$(rollDie)"
       	position=$(($position+$ladderRandom))
-      	echo "Ladder - position is =$position" 
-      	;;
+      	echo "Ladder - position is =$position"
+			if [ $position -gt 100 ]
+			then
+				position=$(($position-$ladderRandom)) 
+      	fi
+			;;
 
    	3)
       	snakeRandom="$(rollDie)"
