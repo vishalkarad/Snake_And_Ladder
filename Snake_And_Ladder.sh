2147fa34baaa627a1cc1ebff620818aa98397334
Your Name <you@example.com>
2020-02-29 12:02:09 +0530

    Added : Check Snake position Ladder position and No Play

--- Snake_And_Ladder.sh
+++ Snake_And_Ladder.sh
@@ -15,3 +15,25 @@ randomNumber=$((RANDOM%6+1))
 echo $randomNumber
 }
 
+# Generate random number 1 to 3
+randomNumber1=$((RANDOM%3+1))
+
+# Check Snake position Ladder position and No Play
+case $randomNumber1 in
+   1)
+      position=$(($position))
+      echo "No Play - position is =$position" 
+      ;;
+
+   2)
+      ladderRandom="$(rollDie)"
+      position=$(($position+$ladderRandom))
+      echo "Ladder - position is =$position" 
+      ;;
+
+   3)
+      snakeRandom="$(rollDie)"
+      position=$(($position-$snakeRandom))
+      echo "Snake - position is =$position" 
+      ;;
+esac
